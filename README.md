3565f5577e3c287ed91b1e2b26cb6947d38b008f
Rio Jos <riojosrj@gmail.com>
2024-07-30 16:10:04 +0530

    cant use usestate - error

--- README.md
+++ README.md
@@ -41,7 +41,7 @@ function App() {
         <Button
           backgroundColor='black'
           color='red'
-          style={{ padding: 20 }}
+          // style={{ padding: 20 }} WRONG !! DO NOT USE STYLE DIRECTLY, IT REWRITES ALL EXISTING JAGGERY DEFINED STYLES
           onClick={ () => alert('Cha-Ching!')}
         >
           Hello Jaggery
@@ -57,3 +57,5 @@ export default App;
 ### why
 
 to have better performance by using highly supported ui/ux components that are minimalistic themed designs, which promotes humane and mindful behaviour
+
+Jaggery will focus on implementing attractive mindful patterns to help build the indieweb, but also not sacrificing on the sweetness
--- src/react/themes/minimalist/components/NavBar/NavBar.tsx
+++ src/react/themes/minimalist/components/NavBar/NavBar.tsx
@@ -1,36 +1,101 @@
-import React from 'react';
+import React, { createElement, useState } from 'react';
+import { Helmet } from 'react-helmet';
 
 export interface INavBarProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
-    backgroundColor?: string;
     color?: string;
+    width?: string;
+    height?: string;
+    backgroundColor?: string;
+    marginTop?: string;
+    boxShadow?: string;
+    backdropFilter?: string;
+    position?: any;
+    display?: string;
+    left?: string;
+    transform?: string;
+    borderRadius?: string;
+    links?: any;
 }
 
-export const NavBar: React.FunctionComponent<INavBarProps> = (props) => {
+export const NavBar: React.FC<INavBarProps> = (props) => {
+    const [hover, setHover] = useState<Boolean>(false);
+
     const {
-        children
-        // backgroundColor, color, style
+        // children,
+        color,
+        width,
+        height,
+        position,
+        display,
+        marginTop,
+        boxShadow,
+        backdropFilter,
+        backgroundColor,
+        left,
+        transform,
+        borderRadius,
+        links
     } = props;
 
-    // let _style: React.CSSProperties = style || {};
     let _style: React.CSSProperties = {
-        width: '100%',
-        height: '100px',
-        marginTop: '0px',
-        boxShadow: '0px 1px 4px #000000',
-        backdropFilter: 'blur(5px)',
-        position: 'fixed',
-        backgroundColor: 'rgba(0, 0, 0, 0.45)'
+        width: width ? width : '90%',
+        height: height ? height : '70px',
+        marginTop: marginTop ? marginTop : '15px',
+        boxShadow: boxShadow ? boxShadow : '0px 0.4px 3px #000000',
+        backdropFilter: backdropFilter ? backdropFilter : 'blur(7px)',
+        position: position ? position : 'fixed',
+        backgroundColor: backgroundColor ? backgroundColor : 'rgba(0, 0, 0, 0.05)',
+        color: color ? color : 'black',
+        display: display ? display : 'flex',
+        left: left ? left : '50%',
+        transform: transform ? transform : 'translate(-50%, 0)',
+        borderRadius: borderRadius ? borderRadius : '1rem',
+        justifyContent: 'center'
     };
 
-    /** Override Defaults */
-    // if (backgroundColor) _style.backgroundColor = backgroundColor;
-    // if (color) _style.color = color;
+    let url_styles: React.CSSProperties = {
+        padding: '21px',
+        fontSize: '16px',
+        fontFamily: '"Exo 2", sans-serif',
+        fontOpticalSizing: 'auto',
+        fontWeight: '10px',
+        fontStyle: 'normal',
+        color: hover ? 'white' : 'black',
+        // color: 'black',
+        textDecoration: 'none'
+    };
 
-    // TODO: make editable css stylings using inline props arguments
-    // ? this should be used as a template for all other components
+    function MouseOver() {
+        setHover(true);
+    }
+    function MouseOut() {
+        setHover(false);
+    }
+
+    const pages = links.map((site: any) => {
+        return createElement(
+            'a',
+            {
+                href: site.url,
+                className: 'nav_urls',
+                key: site.name,
+                style: url_styles,
+                onMouseEnter: MouseOver(),
+                onMouseLeave: MouseOut()
+            },
+            site.name
+        );
+    });
+
+    // TODO: make the component responsive
     return (
         <div style={_style} {...props}>
-            {children}
+            <Helmet>
+                <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+                <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+                <link href="https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"></link>
+            </Helmet>
+            {pages}
         </div>
     );
 };
