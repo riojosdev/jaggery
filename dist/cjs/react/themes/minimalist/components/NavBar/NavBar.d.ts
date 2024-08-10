65d508db8c0c7359225249578a798a43a7a37384
Rio Jos <riojosrj@gmail.com>
2024-08-10 16:53:04 +0530

    re add dist

--- dist/cjs/react/themes/minimalist/components/NavBar/NavBar.d.ts
+++ dist/cjs/react/themes/minimalist/components/NavBar/NavBar.d.ts
@@ -1,6 +1,17 @@
 import React from 'react';
 export interface INavBarProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
-    backgroundColor?: string;
     color?: string;
+    width?: string;
+    height?: string;
+    backgroundColor?: string;
+    marginTop?: string;
+    boxShadow?: string;
+    backdropFilter?: string;
+    position?: any;
+    display?: string;
+    left?: string;
+    transform?: string;
+    borderRadius?: string;
+    links?: any;
 }
-export declare const NavBar: React.FunctionComponent<INavBarProps>;
+export declare const NavBar: React.FC<INavBarProps>;
--- dist/esm/react/themes/minimalist/components/NavBar/NavBar.js
+++ dist/esm/react/themes/minimalist/components/NavBar/NavBar.js
@@ -1,21 +1,64 @@
-import React from 'react';
+import React, { createElement
+// useState
+ } from 'react';
+import { Helmet } from 'react-helmet';
 export const NavBar = (props) => {
-    const { children
-    // backgroundColor, color, style
-     } = props;
-    // let _style: React.CSSProperties = style || {};
+    // const [hover, setHover] = useState<Boolean>(false);
+    const { 
+    // children,
+    color, width, height, position, display, marginTop, boxShadow, 
+    // backdropFilter,
+    backgroundColor, left, transform, borderRadius, links } = props;
     let _style = {
-        width: '100%',
-        height: '100px',
-        marginTop: '0px',
-        boxShadow: '0px 1px 4px #000000',
-        backdropFilter: 'blur(5px)',
-        position: 'fixed',
-        backgroundColor: 'rgba(0, 0, 0, 0.45)'
+        width: width ? width : '85%',
+        height: height ? height : '65px',
+        marginTop: marginTop ? marginTop : '15px',
+        boxShadow: boxShadow ? boxShadow : '2px 3.4px 2px rgba(0, 0, 0, 0.2)',
+        // backdropFilter: backdropFilter ? backdropFilter : 'blur(7px)',
+        position: position ? position : 'fixed',
+        backgroundColor: backgroundColor ? backgroundColor : 'rgba(255, 255, 255, 0.26)',
+        color: color ? color : 'black',
+        display: display ? display : 'flex',
+        left: left ? left : '50%',
+        transform: transform ? transform : 'translate(-50%, 0)',
+        borderRadius: borderRadius ? borderRadius : '0.9rem',
+        justifyContent: 'center'
     };
-    /** Override Defaults */
-    // if (backgroundColor) _style.backgroundColor = backgroundColor;
-    // if (color) _style.color = color;
-    return (React.createElement("div", Object.assign({ style: _style }, props), children));
+    let url_styles = {
+        padding: '21px',
+        fontSize: '16px',
+        fontFamily: '"Exo 2", sans-serif',
+        fontOpticalSizing: 'auto',
+        fontWeight: '10px',
+        fontStyle: 'normal',
+        color: 'black',
+        textDecoration: 'none'
+    };
+    function MouseOver(e) {
+        e.target.style.color = 'white';
+    }
+    function MouseOut(e) {
+        e.target.style.color = 'black';
+    }
+    let pages;
+    if (links) {
+        pages = links.map((site) => {
+            return createElement('a', {
+                href: site.url,
+                className: `nav_urls_${site.name}`,
+                key: site.name,
+                style: url_styles,
+                onMouseEnter: (e) => MouseOver(e),
+                onMouseLeave: (e) => MouseOut(e)
+            }, site.name);
+        });
+    }
+    // TODO: make the component responsive
+    return (React.createElement("div", Object.assign({ style: _style }, props),
+        React.createElement(Helmet, null,
+            React.createElement("link", { rel: "preconnect", href: "https://fonts.googleapis.com" }),
+            React.createElement("link", { rel: "preconnect", href: "https://fonts.gstatic.com" }),
+            React.createElement("link", { href: "https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,100..900;1,100..900&display=swap", rel: "stylesheet" })),
+        pages));
 };
 //# sourceMappingURL=NavBar.js.map
